6ebf53c65108e70be88b249b6a6ea7ac299a7f95
shiv0403 <shiv0403gupta@gmail.com>
2022-07-08 17:26:43 +0530

    cors

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,4 +3,8 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://doubtell-backend.herokuapp.com/",
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
 });
